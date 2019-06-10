0117f982cd425870005ed94c31cf7d1d5e77f774
KimberlySwinton <kimberlyswinton@gmail.com>
2019-06-10 18:39:16 -0400

    Added Import in PostContainer

--- instagram-app/src/components/PostContainer.jsx
+++ instagram-app/src/components/PostContainer.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import CommentSection, {Component} from './CommentSection'
+import CommentSection,  from '../components/CommentSection'
 
 //the main reason to use a class component is to have access to that state
 

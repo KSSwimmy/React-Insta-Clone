9097f89ee99bb90388c73801f84ab2d69122dc53
KimberlySwinton <kimberlyswinton@gmail.com>
2019-06-12 01:34:13 -0400

    Theres an error somewhere in CommentSection

--- instagram-app/src/components/CommentSection.jsx
+++ instagram-app/src/components/CommentSection.jsx
@@ -4,8 +4,29 @@ import React, {Component} from 'react';
 class CommentSection extends Component {
     state = {
         comments: this.props.comments
+        newComment: ''
     }
-    //we're going to add comments to the individual state of each component. 
+    //we're going to add comments: to the individual state of each component.
+
+    addNewComment = e => {
+        e.preventDefault();
+        const newComment = {
+            username: 'KimKimmyKimberly',
+            text: this.state.newComment
+        }
+ 
+        this.setState({ 
+            comments: [...this.state.comments, newComment], 
+            newComment: ''
+         })
+    }
+ 
+    
+    changeHandler = event => {
+        this.setState({
+         [e.target.maps]: event.target.value
+        })
+    }
     
     render() {
         return (
@@ -23,11 +44,19 @@ class CommentSection extends Component {
                 )
             })}
             
-             <p className="timestamp">{this.props.timestamp}</p>
-             <input type="text" placeholder="add a comment..."/>
-            </div>
+            <p className="timestamp">{this.props.timestamp}</p>
+            <form onSubmit={this.addNewComment}>
+                <input 
+                type="text" 
+                name="newComment" 
+                value={this.state.newComment}
+                onChange={this.changeHandler}
+                placeholder="add a comment..."
+            />
+            </form>
+        </div> 
         );
-    }
+}
 }
 
 export default CommentSection;
